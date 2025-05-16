423eacb68ad7e7e43c4dfc1cbeec63014bbe2c46
RicardoMota22 <ricardomota.artist@gmail.com>
2025-05-16 09:42:52 +0100

    add Controller.cs

---
+++ PlayerManagerMVC/PlayersList.cs
@@ -0,0 +1,25 @@
+
+using System.Collections.Generic;
+
+
+namespace PlayerManagerMVC
+{
+    public class PlayersList : List<Player>
+    {
+       public IEnumerable<Player> GetPlayersWithScoreGreaterThan(int minScore)
+        {
+            // Cycle all players in the original player list
+            foreach (Player p in playerList)
+            {
+                // If the current player has a score higher than the
+                // given value....
+                if (p.Score > minScore)
+                {
+                    // ...return him as a member of the player enumerable
+                    yield return p;
+                }
+            }
+        }
+    }
+   
+}
