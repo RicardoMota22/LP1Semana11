2e951c9a176766e4b3d74103dfb5505d8a498978
RicardoMota22 <ricardomota.artist@gmail.com>
2025-05-16 17:01:59 +0100

    add interface IView and class UglyView

---
+++ GuessTheWord/UglyView.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace GuessTheWord
+{
+    public class UglyView : IView
+    {
+        public string DictionaryMenu()
+        {
+            { "apple", "fruit" };
+            { "banana", "fruit" };
+            { "tiger", "animal" };
+            { "elephant", "animal" };
+            { "guitar", "instrument" };
+            { "violin", "instrument" };
+            { "canada", "country" };
+            { "brazil", "country" };
+            { "laptop", "object" };
+            { "microscope", "scientific instrument" }
+        }
+    }
+}
